d97a5f5830f7b15d781ab510d8a84725956a1cba
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-02-02 17:05:27 +0900

    Add run.sh
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd `dirname $` && pwd)
+
+# Setup python virtual environment
+cd ${SCRIPT_DIR}
+if [[ ! -e "${SCRIPT_DIR}/labgrid-venv" ]]; then
+    python3 -m venv ${SCRIPT_DIR}/labgrid-venv
+    source ${SCRIPT_DIR}/labgrid-venv/bin/activate
+    pip install --upgrade pip
+    pip install --upgrade labgrid
+else
+    source ${SCRIPT_DIR}/labgrid-venv/bin/activate
+fi
+
+# Run testsuite
+cd ${SCRIPT_DIR}/yocto
+pytest --lg-env local.yaml --capture=no test_shell.py
+
