af95badb55e404e20908744228f7cf68e70d8dfa
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2026-02-02 19:52:43 +0900

    yocto: Prepare uboot test support
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- yocto/conftest.py
+++ yocto/conftest.py
@@ -1,8 +1,14 @@
 import pytest
 
-
 @pytest.fixture(scope="session")
 def command(target):
-    shell = target.get_driver("CommandProtocol")
+    shell = target.get_driver("ShellDriver")
     target.activate(shell)
     return shell
+
+@pytest.fixture(scope="session")
+def uboot_command(target):
+    shell = target.get_driver("UBootDriver")
+    target.activate(shell)
+    return shell
+
